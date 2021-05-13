58a95545c3985687b53ff6d81e3306e6f00cd9bd
Anya Kovalieva <annakovalieva@gmail.com>
2021-05-13 11:55:47 +0200

    added db entities - not integrated

---
+++ src/Entity/Score.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScoreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ScoreRepository::class)
+ */
+class Score
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $playerName;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $score;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $game;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlayerName(): ?string
+    {
+        return $this->playerName;
+    }
+
+    public function setPlayerName(string $playerName): self
+    {
+        $this->playerName = $playerName;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getGame(): ?string
+    {
+        return $this->game;
+    }
+
+    public function setGame(string $game): self
+    {
+        $this->game = $game;
+
+        return $this;
+    }
+}
---
+++ src/Repository/ScoreRepository.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Score;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Score|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Score|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Score[]    findAll()
+ * @method Score[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ScoreRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Score::class);
+    }
+
+    /**
+     * @return Score[]
+     */
+    public function showAllSortedDesc(string $game): array
+    {
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQuery(
+            'SELECT s
+            FROM App\Entity\Score s
+            WHERE s.game = :game
+            ORDER BY s.score DESC
+            '
+        )->setParameter('game', $game);
+        $query->setMaxResults(10);
+        // returns an array of Product objects
+        return $query->getResult();
+    }
+
+    // /**
+    //  * @return Score[] Returns an array of Score objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Score
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
